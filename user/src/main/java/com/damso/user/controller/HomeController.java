c009a009adc606b6d12cac65b3ff8001cb274599
alex <rudtjq1213@saramin.co.kr>
2024-12-05 15:07:22 +0900

    feat: sp-202412 [User] 사용자 페이지 구성 추가
    홈과 로그인 페이지 추가
    todo 메뉴 css 수정 필요

--- user/src/main/java/com/damso/user/controller/HomeController.java
+++ user/src/main/java/com/damso/user/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.damso.admin.controller;
+package com.damso.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
