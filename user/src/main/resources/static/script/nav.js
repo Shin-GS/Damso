c6399962cc2693831beff29e5eda5fb20a115e44
alex <rudtjq1213@saramin.co.kr>
2024-12-20 15:01:45 +0900

    feat: sp-202412 메뉴 노출 방식 변경
    햄버거 메뉴 -> 화면 크기가 작아지면 아이콘 노출로 변경

--- user/src/main/resources/static/script/nav.js
+++ user/src/main/resources/static/script/nav.js
@@ -1,7 +1,6 @@
-import {fetchWithCredentials} from "./customFetch.js";
+import { fetchWithCredentials } from "./customFetch.js";
 
 document.addEventListener('DOMContentLoaded', function () {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');
     const navMenu = document.querySelector('.nav-menu');
     const loginButton = document.getElementById('login-button');
     const userInfo = document.getElementById('user-info');
@@ -9,35 +8,33 @@ document.addEventListener('DOMContentLoaded', function () {
     const userName = document.getElementById('user-name');
     const userSubmenu = userInfo.querySelector('.user-submenu');
 
-    hamburgerMenu.addEventListener('click', function () {
-        navMenu.classList.toggle('active');
-    });
-
+    // 사용자 정보 드롭다운 메뉴 토글
     userInfo.addEventListener('click', function () {
         userSubmenu.style.display = userSubmenu.style.display === 'block' ? 'none' : 'block';
     });
 
+    // URL에서 auth 및 refresh 토큰 가져오기 및 저장
     function fetchToken() {
         const urlParams = new URLSearchParams(window.location.search);
 
-        // URL을 통해 auth 토큰 세팅
+        // auth 토큰 저장
         const auth = urlParams.get('auth');
         if (auth) {
             localStorage.setItem('auth', auth);
         }
 
-        // URL을 통해 refresh 토큰 세팅
+        // refresh 토큰 저장
         const refresh = urlParams.get('refresh');
         if (refresh) {
             localStorage.setItem('refresh', refresh);
         }
 
-        // URL에서 토큰 정보 제거
+        // URL 정리
         const cleanUrl = window.location.origin + window.location.pathname;
         window.history.replaceState({}, document.title, cleanUrl);
     }
 
-    // API를 통해 회원 정보 세팅
+    // API 호출로 사용자 정보 가져오기
     function fetchMemberInfo() {
         const authToken = localStorage.getItem("auth");
         if (!authToken) {
@@ -59,8 +56,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 // 사용자 정보 표시
                 userInfo.style.display = 'flex';
-                // profilePicture.src = data.profilePicture || '/default-profile.png';
-                profilePicture.src = '/favicon.ico';
+                profilePicture.src = data.profilePicture || '/favicon.ico';
                 userName.textContent = data.name || '회원님';
             })
             .catch(error => {
@@ -68,6 +64,7 @@ document.addEventListener('DOMContentLoaded', function () {
             });
     }
 
+    // 초기화 함수 호출
     fetchToken();
     fetchMemberInfo();
 });
