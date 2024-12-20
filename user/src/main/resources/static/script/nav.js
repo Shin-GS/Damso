c1d5872245b071d46a11b2420fa35f713b559594
alex <rudtjq1213@saramin.co.kr>
2024-12-20 14:26:00 +0900

    feat: sp-202412 개인정보 새로고침 기능 추가
    화면 구현

--- user/src/main/resources/static/script/nav.js
+++ user/src/main/resources/static/script/nav.js
@@ -3,21 +3,30 @@ import {fetchWithCredentials} from "./customFetch.js";
 document.addEventListener('DOMContentLoaded', function () {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const navMenu = document.querySelector('.nav-menu');
+    const loginButton = document.getElementById('login-button');
+    const userInfo = document.getElementById('user-info');
+    const profilePicture = document.getElementById('profile-picture');
+    const userName = document.getElementById('user-name');
+    const userSubmenu = userInfo.querySelector('.user-submenu');
 
     hamburgerMenu.addEventListener('click', function () {
         navMenu.classList.toggle('active');
     });
 
+    userInfo.addEventListener('click', function () {
+        userSubmenu.style.display = userSubmenu.style.display === 'block' ? 'none' : 'block';
+    });
+
     function fetchToken() {
         const urlParams = new URLSearchParams(window.location.search);
 
-        //url을 통해 auth 토큰 세팅
+        // URL을 통해 auth 토큰 세팅
         const auth = urlParams.get('auth');
         if (auth) {
             localStorage.setItem('auth', auth);
         }
 
-        //url을 통해 refresh 토큰 세팅
+        // URL을 통해 refresh 토큰 세팅
         const refresh = urlParams.get('refresh');
         if (refresh) {
             localStorage.setItem('refresh', refresh);
@@ -28,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function () {
         window.history.replaceState({}, document.title, cleanUrl);
     }
 
-    //api를 통해 회원정보 세팅
+    // API를 통해 회원 정보 세팅
     function fetchMemberInfo() {
         const authToken = localStorage.getItem("auth");
         if (!authToken) {
@@ -36,9 +45,23 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         fetchWithCredentials('/api/member/refresh-info')
-            .then(response => response.json())
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to fetch member info');
+                }
+                return response.json();
+            })
             .then(data => {
                 console.log('Member Info:', data);
+
+                // 로그인 버튼 숨기기
+                loginButton.style.display = 'none';
+
+                // 사용자 정보 표시
+                userInfo.style.display = 'flex';
+                // profilePicture.src = data.profilePicture || '/default-profile.png';
+                profilePicture.src = '/favicon.ico';
+                userName.textContent = data.name || '회원님';
             })
             .catch(error => {
                 console.error('Error fetching member info:', error);
