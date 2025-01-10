3b079bbb7422865030ce136db170ca19f3296f60
alex <rudtjq1213@saramin.co.kr>
2025-01-10 17:21:59 +0900

    feat: sp-202501 GlobalControllerExceptionHandler 추가
    GlobalControllerExceptionHandler 추가
    (controller, hx에서 에러 발생시 json으로 리턴)
    
    GlobalExceptionHandler -> GlobalRestControllerExceptionHandler

---
+++ core/src/main/java/com/damso/core/response/exception/GlobalRestControllerExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.damso.core.response.exception;
+
+import com.damso.core.response.error.ErrorCode;
+import com.damso.core.response.error.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalRestControllerExceptionHandler {
+    private GlobalRestControllerExceptionHandler() {
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("handle MethodArgumentNotValidException: ", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BusinessException.class)
+    protected ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        log.error("handle BusinessException: ", e);
+        ErrorResponse errorResponse = StringUtils.hasText(e.getMessage()) ? ErrorResponse.of(e.getCode(), e.getMessage()) : ErrorResponse.of(e.getCode());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    protected ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("handle error: ", e);
+        ErrorResponse response = ErrorResponse.of(ErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
