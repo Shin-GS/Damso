6cabf17fde20d81776f548607fa91cfe009ce3d5
DESKTOP-T72GEF9\user <rudtjq0842@naver.com>
2025-05-21 16:33:47 +0900

    feat: sp-202505 댓글
    댓글 작성을 위한 정규식 추가

--- application/common/src/main/java/com/damso/common/request/pattern/StoryRegexPattern.java
+++ application/common/src/main/java/com/damso/common/request/pattern/StoryRegexPattern.java
@@ -9,7 +9,7 @@ public enum StoryRegexPattern implements CommonRegexPattern {
     TITLE("TITLE", "^[a-zA-Z0-9\\s\\-가-힣ㄱ-ㅎㅏ-ㅣ]{1,255}$", "^[a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ0-9\\s\\-]{1,255}$", "제목은 공백, 한글, 영문, 숫자, '-'만 허용됩니다"),
     STORY_TYPE("STORY_TYPE", "", "", "스토리 유형을 선택해주세요"),
     COMMENT_TYPE("COMMENT_TYPE", "", "", "댓글 유형을 선택해주세요"),
-    COMMENT_TEXT("COMMENT_TEXT", "", "", "댓글을 입력해주세요"),
+    COMMENT_TEXT("COMMENT_TEXT", "^[가-힣a-zA-Z0-9.,!?()\\\\[\\\\]\\\\-_'\\\"\\\\s]{1,500}$", "", "댓글을 입력해주세요"),
     STORY_ID("STORY_ID", "", "", "스토리 번호를 입력해주세요"),
     PAGE_ID("PAGE_ID", "", "", "페이지 번호를 입력해주세요");
 
